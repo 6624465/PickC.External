2f88696dfac9a5c6d3f0e865f9abd3983a7154be
vijay <vijay@sulabs.tech>
2018-01-06 11:21:51 +0530

    Add project files.

---
+++ PickC.External/Controllers/DashboardController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PickC.External.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult CustomerSupport()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult Faqs()
+        {
+            return View();
+        }
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+        public ActionResult TermsAndConditions()
+        {
+            return View();
+        }
+        public ActionResult Careers()
+        {
+            return View();
+        }
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
